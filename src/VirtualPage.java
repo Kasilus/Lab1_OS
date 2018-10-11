c55dacb4433537211ce52bee1486971b5aba5d97
Kasilus <its.an.omen.man@gmail.com>
2018-10-12 01:54:06 +0300

    Structure of virtual page record

---
+++ src/VirtualPage.java
@@ -0,0 +1,48 @@
+public class VirtualPage {
+
+    private Boolean precedenceBit;
+    private Boolean modificationBit;
+    private Boolean readBit;
+    private Boolean referencedBit;
+    private PhysicalPage physicalPage;
+
+    public Boolean getPrecedenceBit() {
+        return precedenceBit;
+    }
+
+    public void setPrecedenceBit(Boolean precedenceBit) {
+        this.precedenceBit = precedenceBit;
+    }
+
+    public Boolean getModificationBit() {
+        return modificationBit;
+    }
+
+    public void setModificationBit(Boolean modificationBit) {
+        this.modificationBit = modificationBit;
+    }
+
+    public Boolean getReadBit() {
+        return readBit;
+    }
+
+    public void setReadBit(Boolean readBit) {
+        this.readBit = readBit;
+    }
+
+    public Boolean getReferencedBit() {
+        return referencedBit;
+    }
+
+    public void setReferencedBit(Boolean referencedBit) {
+        this.referencedBit = referencedBit;
+    }
+
+    public PhysicalPage getPhysicalPage() {
+        return physicalPage;
+    }
+
+    public void setPhysicalPage(PhysicalPage physicalPage) {
+        this.physicalPage = physicalPage;
+    }
+}
