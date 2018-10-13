0c0d561299f068ab5689ccbda94924be3306a371
Kasilus <its.an.omen.man@gmail.com>
2018-10-13 16:33:58 +0300

    Create some processes and specify start virtual memory for them

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,10 +1,22 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Lab1 {
+
     public static void main(String[] args) {
 
         OS os = new OS();
         List<PhysicalPage> physicalPages = os.getPhysicalPages();
+        // Static number of processes. Maybe, latter it will be dynamic number with process killing and init. ahah-ahahah
+        Integer numberOfProcesses = 3;
+        // TODO: replace with smth more comfortable
+        List<Runnable> processes = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < numberOfProcesses; i++) {
+            Integer needMemoryForProcessAtStart = random.nextInt(OS.VARIABLES.PHYSICAL_PAGE_AMOUNT * 5 / 4);
+            processes.add(new Process(i, needMemoryForProcessAtStart));
+        }
 
     }
 }
